bdbc45dd3d045aa8ba912e98dfaf4047497b1e4e
李祥 <lixiang@tzt.cn>
2019-04-08 11:30:17 +0800

    test submit

---
+++ Example/Pods/Target Support Files/FFCategaryKitLXNew/FFCategaryKitLXNew-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FFCategaryKitLXNew : NSObject
+@end
+@implementation PodsDummy_FFCategaryKitLXNew
+@end
---
+++ Example/Pods/Target Support Files/Pods-FFCategaryKitLXNew_Example/Pods-FFCategaryKitLXNew_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FFCategaryKitLXNew_Example : NSObject
+@end
+@implementation PodsDummy_Pods_FFCategaryKitLXNew_Example
+@end
